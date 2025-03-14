230af7f7cabac27131dec343bc91343c180f6cfd
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-03-14 17:54:03 -0400

    KeyHandler: Fix "overrides nothing" build error

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -165,9 +165,9 @@ class KeyHandler(private val context: Context) : DeviceKeyHandler {
         return null
     }
 
-    override fun onPocketStateChanged(inPocket: Boolean) {
-        // Do nothing
-    }
+    //override fun onPocketStateChanged(inPocket: Boolean) {
+    //    // Do nothing
+    //}
 
 
     private fun doHapticFeedback(effect: Int) {
